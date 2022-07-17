244647d53e88c0ac97cce2746ffb40a241a27dca
Anatoly <podyapolskiyaa@gmail.com>
2022-07-17 14:36:56 +0300

    Add index_page

--- cottagevillagemayak/mayak/urls.py
+++ cottagevillagemayak/mayak/urls.py
@@ -16,6 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+from crm.views import index_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_page)
 ]
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # + \
+# static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# For static in development
