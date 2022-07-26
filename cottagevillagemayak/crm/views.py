20fea9d6f04e02df965ba2fcf23405f793173488
Anatoly <podyapolskiyaa@gmail.com>
2022-07-26 14:18:41 +0300

    Create custom range filter; Register about page;

--- cottagevillagemayak/crm/views.py
+++ cottagevillagemayak/crm/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
+from django.template.defaulttags import register
+
+
+@register.filter
+def get_range(value):
+    """Custom jinja filter that accepts usage of range loop in jinja.
+
+    Args:
+        value int: number of iterations
+
+    Returns:
+        range: range object
+
+    Example:
+        >>> {% for i in 5|get_range %}
+        >>>     {{ i }}
+        >>> {% endfor %}
+
+    Result:
+        >>> 0
+        >>> 1
+        >>> 2
+        >>> 3
+        >>> 4
+    """
+    return range(value)
 
 
 def index_page(request):
     return render(request, './index.html')
+
+
+def about_page(request):
+    return render(request, './about.html')
